91f254ad3dab360a9d9cc3bb0a17e4771b042c47
Shawn Lim Ke Qin <e0052482@u.nus.edu>
2020-05-23 02:20:50 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {useSelector , useDispatch} from 'react-redux'
+import {increment, decrement} from './actions/index'
+
+function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
+  return (
+    <div className="App">
+      <h1>Counter: {counter}</h1>
+      <button onClick = {() => dispatch(increment(5))}>+</button>
+      <button onClick = {() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3> valuable information i shouldn't see </h3> : ''}
+    </div>
+  );
+}
+
+export default App;
