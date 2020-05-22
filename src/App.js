8ba4b2e783e262799492793c2931b9e1a9de6446
Shawn <devcorehd@gmail.com>
2020-05-23 01:19:51 +0800

    adding files

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Posts from './components/Posts'
+import PostForm from './components/Postform'
+import { Provider } from 'react-redux'
+import store from './store'
+ 
+function App() {
+  return (
+    <Provider store = {store}>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <PostForm /> 
+        <hr /> 
+       <Posts /> 
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
